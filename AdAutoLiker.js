a88c56e56c0b58dbbb4442b5ae1f11823ee2786a
michalanisimow <M.anisimow@liverpool.ac.uk>
2022-02-21 19:28:53 +0000

    ad auto liker v02

--- AdAutoLiker.js
+++ AdAutoLiker.js
@@ -1,27 +1,30 @@
 // ==UserScript==
-// @name         PAW - Ad Auto Liker
+// @name         PNW - Ad Auto Liker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  Automatically likes ad from the AllianceLeaderName.
 // @author       You
 // @match        https://politicsandwar.com/*
 // @icon         https://www.google.com/s2/favicons?domain=politicsandwar.com
+// @updateURL   https://raw.githubusercontent.com/michalani/PNW-Scripts/master/AdAutoLiker.js
 // @grant        none
 // ==/UserScript==
 
+let AllianceLeaderName = 'Edward the Elder';
+
 setTimeout(
     function() {
-        if(document.querySelector('div.hidden-xs.alert.alert-info p.bold a').textContent == 'Edward the Elder')
+        if(document.querySelector('div.hidden-xs.alert.alert-info p.bold a').textContent == AllianceLeaderName)
             try {
                 document.querySelector('.btn-ad-vote.btn-default').click()
-                console.log('Successfully like the ad!');
+                //console.log('Successfully like the ad!');
             }
         catch (exception_var) {
             if(document.querySelector('.btn-ad-vote.btn-success' != undefined)){
-                console.log('Ad was already liked.')
+                //console.log('Ad was already liked.')
             } else{
-                console.log('Unexpected error! in Ad Liker..')
-                console.log(exception_var);
+                //console.log('Unexpected error! in Ad Liker..')
+                //console.log(exception_var);
             }
         }
     }, 2000);
